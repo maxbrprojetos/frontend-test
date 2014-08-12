96340a36fb8d0698395d058aaf50c4bc277108be
Guilherme Pontes <guisantos@sp.r7.com>
2014-08-12 15:11:34 -0300

    cleaning the app

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var app = require('koa')();
 var common = require('koa-common');
 
-app.use(common.logger('dev'));
-
 app.use(common.static(__dirname + '/public'));
 
-app.use(function *() {
-  this.body = "Hello World";
-});
-
-var server = app.listen(4000, function () {
-  console.log('Listening on port %d', server.address().port);
+var server = app.listen(8000, function () {
+  console.log(' ➜  Listening on localhost:%d', server.address().port);
 });
